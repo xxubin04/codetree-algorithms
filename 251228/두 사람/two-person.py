39b843753119eaac0c88b2b1d1834b5c65447ef3
정유빈 <127945869+xxubin04@users.noreply.github.com>
2025-12-28 20:30:23 +0900

    Title: 두 사람, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 251228/두 사람/two-person.py
+++ 251228/두 사람/two-person.py
@@ -2,7 +2,7 @@ import sys
 a, a_s = sys.stdin.readline().strip().split()
 b, b_s = sys.stdin.readline().strip().split()
 
-if (a_s == 'M' and a >= '19') or (b_s == 'M' and b >= '19'):
+if (a_s == 'M' and int(a) >= 19) or (b_s == 'M' and int(b) >= 19):
     print(1)
 else:
     print(0)
